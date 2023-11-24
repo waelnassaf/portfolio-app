dfccca4ce48c1a8e21ad7849d436f4f850bb7fba
Wael <waelassaf121@gmail.com>
2023-11-25 00:42:24 +0300

    Initial Push

---
+++ components/Projects.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React from "react"
+import { SectionHeading, Project } from "@/components"
+import { projectsData } from "@/lib/data"
+import { useSectionInView } from "@/lib/hooks"
+
+const Projects = () => {
+    const { ref } = useSectionInView("Projects", 0.5)
+
+    return (
+        <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+            <SectionHeading>My projects</SectionHeading>
+            <div>
+                {projectsData.map((project, index) => (
+                    <React.Fragment key={index}>
+                        <Project {...project} />
+                    </React.Fragment>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Projects
---
+++ components/index.ts
@@ -0,0 +1,29 @@
+import Header from "./Header"
+import Intro from "./Intro"
+import SectionDivider from "./SectionDivider"
+import SectionHeading from "./SectionHeading"
+import About from "./About"
+import Projects from "./Projects"
+import Project from "./Project"
+import Skills from "./Skills"
+import Experience from "./Experience"
+import Contact from "./Contact"
+import SubmitButton from "./SubmitButton"
+import Footer from "./Footer"
+import ThemeSwitch from "./ThemeSwitch"
+
+export {
+    Header,
+    Intro,
+    SectionDivider,
+    SectionHeading,
+    About,
+    Projects,
+    Project,
+    Skills,
+    Experience,
+    Contact,
+    SubmitButton,
+    Footer,
+    ThemeSwitch,
+}
